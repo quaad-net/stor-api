c008fd72cc6a582071e9deecf7b9f629e69bd98f
unknown <eukoh@quaad.net>
2025-01-20 20:55:51 -0600

    ...

--- server.js
+++ server.js
@@ -1,17 +1,14 @@
 import express from "express";
 import cors from "cors";
 import apiRouter from "./routes/api.js"
-import rootRouter from "./routes/root.js"
 
 const PORT = process.env.PORT || 5050;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use("/api", apiRouter);
-app.use("/", rootRouter);
+app.use("/", apiRouter);
 
-// start the Express server
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });

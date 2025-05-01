9678088e0ac1948d7c5353512274373b4527b116
unknown <eukoh@quaad.net>
2025-05-01 12:21:15 -0500

    Added uwmBDops

---
+++ db/uwmDBops.js
@@ -0,0 +1,163 @@
+import { MongoClient } from "mongodb";
+import 'dotenv/config';
+
+const uri = process.env.ATLAS_URI;
+const client = new MongoClient(uri);
+
+export async function deletePartUsageRecords(){
+
+  try{
+    await client.connect();
+    const clientDeletes = 
+    [
+      {
+        namespace: "quaad.uwm_part_usage",
+        name: "deleteMany",
+        filter: {}
+      }, 
+      {
+        namespace: "quaad.uwm_part_usage_122_90",
+        name: "deleteMany",
+        filter: {}
+      }, 
+      {
+        namespace: "quaad.uwm_part_usage_91_59",
+        name: "deleteMany",
+        filter: {}
+      }, 
+      {
+        namespace: "quaad.uwm_part_usage_60_28",
+        name: "deleteMany",
+        filter: {}
+      }, 
+    ]
+    
+    const clientDeleteRes = await client.bulkWrite(clientDeletes);
+    console.log(`Deleted documents: ${clientDeleteRes.deletedCount}`);
+  }
+  finally{
+    client.close()
+  }
+}
+
+export async function updatePartUsageAnalysis(){
+    try{
+        await client.connect();
+        const database = client.db('quaad');
+        const inventoryColl = database.collection('uwm_inventory');
+        const leadTimeColl = database.collection('uwm_high_lead_time_parts');
+        const usageColl = database.collection('uwm_part_usage');
+        const analysisColl = database.collection('uwm_part_usage_analysis');
+        const usageAnalysisMissed = database.collection('uwm_part_usage_analysis_missed');
+        const usageP1Coll = database.collection('uwm_part_usage_122_90');
+        const usageP2Coll = database.collection('uwm_part_usage_91_59');
+        const usageP3Coll = database.collection('uwm_part_usage_60_28');
+        await analysisColl.deleteMany({})
+        const distinctParts = await usageColl.distinct('materialPartCode', {})
+
+        for await (const part of distinctParts){
+          let total90DayUsage = 0;
+          let p1Usage = 0; // -122 days < part usage > -90 days
+          let p2Usage = 0; // -91 days < part usage > -59 days
+          let p3Usage = 0; // -60 days < part usage > -28 days
+          let min = 0;
+          let max = 0;
+          let leadTime = 14;
+          let avgDailyUsage = 0;
+          const occurances =  await usageColl.find({materialPartCode: part}).project({_id: 0, materialQuantity: 1}).toArray();
+          occurances.forEach((o)=>{
+              total90DayUsage += o.materialQuantity
+          })
+          avgDailyUsage = total90DayUsage / 90; 
+          //codemark
+          function addDays(date, days) {
+            let result = new Date(date);
+            result.setDate(result.getDate() + days);
+            return result;
+          }
+          const p1occurances = await usageP1Coll.find({materialPartCode: part}).project({_id: 0, materialQuantity: 1 }).toArray();
+          p1occurances.forEach((o)=>{
+            p1Usage += o.materialQuantity
+          })
+
+          const p2occurances = await usageP2Coll.find({materialPartCode: part}).project({_id: 0, materialQuantity: 1 }).toArray();
+          p2occurances.forEach((o)=>{
+            p2Usage += o.materialQuantity
+          })
+
+          const p3occurances = await usageP3Coll.find({materialPartCode: part}).project({_id: 0, materialQuantity: 1 }).toArray();
+          p3occurances.forEach((o)=>{
+            p3Usage += o.materialQuantity
+          })
+
+          const partArr = part.split('-');
+          if(partArr.length == 3){
+              const simpleCode = partArr[0] + '-' + partArr[1];
+              const warehouseCode = partArr[2];
+              const inventoryRec = await inventoryColl.find({code: simpleCode, warehouseCode: warehouseCode}).toArray()
+              if(inventoryRec.length == 1){
+                  const partInventory  = inventoryRec[0];
+                  min = partInventory.min
+                  max = partInventory.max
+                  const leadTimeRec = await leadTimeColl.find({part_code: simpleCode}).toArray()
+                  if(leadTimeRec == 1){
+                      leadTime = leadTimeRec[0].est_lead_time_days
+                  }
+              }
+              else{
+                usageAnalysisMissed.insertOne({_id: part});
+                continue
+              }
+          }
+          else{
+            usageAnalysisMissed.insertOne({_id: part});
+            continue
+          }
+          const suggestedMin =  Number((leadTime * avgDailyUsage).toFixed(0));
+          const analysis = {
+              code: part,
+              leadTime: leadTime,
+              min: min,
+              max: max,
+              avgDailyUsage: avgDailyUsage.toFixed(2),
+              total90DayUsage: total90DayUsage,
+              p1Usage: p1Usage,
+              p2Usage: p2Usage,
+              p3Usage: p3Usage,
+              increaseSchema: ((leadTime * avgDailyUsage) > Number(min) + 1), // prev: Number(min) != 0 ? ((leadTime * avgDailyUsage) > Number(min)) : false
+              decreaseSchema: (2 * (leadTime * avgDailyUsage) < Number(min)), // Number(min) != 0 ? (2 * (leadTime * avgDailyUsage) < Number(min)) : false
+              suggestedMin: suggestedMin,
+              analysisDate: new Date().toLocaleDateString()
+          }
+          await analysisColl.insertOne(analysis)
+        }
+    }
+    finally{
+        await client.close();
+    }
+} 
+
+export async function updateLeadTimes(){
+    // Updates uwm_inventory with leadTime from uwm_high_lead_time_parts. 
+    try{
+      await client.connect();
+      const database = client.db('quaad');
+      const leadColl = database.collection('uwm_high_lead_time_parts'); 
+      const highLeads = await leadColl.find({}).toArray();
+      const mainColl = database.collection('uwm_inventory');
+      await mainColl.updateMany({}, {$set: {leadTime: 14}});
+      async function updateLeadTime(filter, updateDoc){
+        const result = await mainColl.updateMany(filter, updateDoc);
+        console.log(`Updated ${result.modifiedCount} record(s).`)
+      }
+      highLeads.forEach((lead)=>{
+        const updateDoc = {
+          $set: {leadTime: lead.est_lead_time_days}
+        }
+        updateLeadTime({code: lead.part_code}, updateDoc)
+      })
+    }finally{
+      await client.close();
+    }
+  }
+
--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,7 @@ import express, { json, Router } from "express";
 import {db, client} from "../db/connection.js";
 import auth from "../auth.js";
 import { ObjectId } from "mongodb";
+import { connect } from "mongoose";
 
 const router = express.Router();
 
@@ -244,6 +245,26 @@ router.post("/inventory/partCode", auth, async (req, res)=>{
 
 // })
 
+router.get('/inventory/usage_analysis/:institution/:partcode', async(req, res)=>{
+    try{
+        await client.connect();
+        const database = client.db('quaad');
+        const institution  = req.params.institution;
+        const coll = database.collection(`${institution}_part_usage_analysis`);
+        const partCode = req.params.partcode;
+        const usage = await coll.find({code: partCode}).toArray();
+        if(usage.length == 1){
+            res.json(usage[0])
+        }
+        else{
+            throw new Error('Not found')
+        }
+    }
+    catch(err){
+        res.status(500).json({message: 'Could not fetch usage'})
+    }
+})
+
 router.post('/inventory_count', auth, async(req, res)=>{
     try {
         await client.connect();
@@ -413,93 +434,93 @@ router.post('/inventory_tasks_print/get-all', auth, async(req, res)=>{
 })
 
 
-// Get part records for labels using warehouseBinLocation query.
-router.post("/labels/:query", auth, async (req, res)=>{
+// // Get part records for labels using warehouseBinLocation query.
+// router.post("/labels/:query", auth, async (req, res)=>{
     
-    try{
-        if(req.params.query == ""){throw new Error('Invalid query format')}
-        const queryStr = req.params.query.trim().toUpperCase();
-        const queryArr = [...queryStr];
-        const active = /&ACTIVE/ // Returns only active parts if present in query
-        const isActive = active.test(queryStr);
-        let colonCount = 0;
-        queryArr.map((str)=>{
-            if(str==':'){
-                colonCount += 1;
-            }
-        })
-        if(colonCount == 1){ // Indicates a range of bin locations.
-            const querySplit = queryStr.split(":");
-            const startQryAt = querySplit[0];
-            let endQryAt; 
-            if(isActive){endQryAt =  querySplit[1].replace('&ACTIVE', '').trim()}
-            else{ endQryAt = querySplit[1]}
-            await client.connect();
-            const coll = db.collection("uwm_stor_parts");
-            const result = await coll.find( { warehouseBinLocation: { $gte: startQryAt, $lte: endQryAt }, ...(isActive ? { active: 'True' } : {}) } )
-            .sort({warehouseBinLocation: 1, partCode: 1}).toArray();
-            res.json(result);
-        }
-        else{
-            if(colonCount == 0){
-                await client.connect();
-                const coll = db.collection("uwm_stor_parts");
-                const result = await coll.find( { warehouseBinLocation: queryStr, ...(isActive ? { active: 'True' } : {})} )
-                .sort({warehouseBinLocation: 1, partCode: 1}).toArray();
-                res.json(result);
-            }
-            else{throw new Error('Invalid query format')}
-        }
+//     try{
+//         if(req.params.query == ""){throw new Error('Invalid query format')}
+//         const queryStr = req.params.query.trim().toUpperCase();
+//         const queryArr = [...queryStr];
+//         const active = /&ACTIVE/ // Returns only active parts if present in query
+//         const isActive = active.test(queryStr);
+//         let colonCount = 0;
+//         queryArr.map((str)=>{
+//             if(str==':'){
+//                 colonCount += 1;
+//             }
+//         })
+//         if(colonCount == 1){ // Indicates a range of bin locations.
+//             const querySplit = queryStr.split(":");
+//             const startQryAt = querySplit[0];
+//             let endQryAt; 
+//             if(isActive){endQryAt =  querySplit[1].replace('&ACTIVE', '').trim()}
+//             else{ endQryAt = querySplit[1]}
+//             await client.connect();
+//             const coll = db.collection("uwm_stor_parts");
+//             const result = await coll.find( { warehouseBinLocation: { $gte: startQryAt, $lte: endQryAt }, ...(isActive ? { active: 'True' } : {}) } )
+//             .sort({warehouseBinLocation: 1, partCode: 1}).toArray();
+//             res.json(result);
+//         }
+//         else{
+//             if(colonCount == 0){
+//                 await client.connect();
+//                 const coll = db.collection("uwm_stor_parts");
+//                 const result = await coll.find( { warehouseBinLocation: queryStr, ...(isActive ? { active: 'True' } : {})} )
+//                 .sort({warehouseBinLocation: 1, partCode: 1}).toArray();
+//                 res.json(result);
+//             }
+//             else{throw new Error('Invalid query format')}
+//         }
 
-    }
-    catch(err){
-        if(err.message == 'Invalid query format'){
-            res.status(400).json({message: "Invalid syntax"})
-        }
-        else{res.status(500).json({message:"Error fetching data"})}
-    }
-})
+//     }
+//     catch(err){
+//         if(err.message == 'Invalid query format'){
+//             res.status(400).json({message: "Invalid syntax"})
+//         }
+//         else{res.status(500).json({message:"Error fetching data"})}
+//     }
+// })
 
-// Get part records for labels using partCode query.
-router.post("/labels/partcode/:partcode", auth, async (req, res)=>{
+// // Get part records for labels using partCode query.
+// router.post("/labels/partcode/:partcode", auth, async (req, res)=>{
  
-    // Note: :partcode param can contain multiple parts seperated by " ".
+//     // Note: :partcode param can contain multiple parts seperated by " ".
 
-    try{
+//     try{
 
-        if(req.params.partcode == ""){throw new Error('Invalid query format')}
+//         if(req.params.partcode == ""){throw new Error('Invalid query format')}
 
-        const part = req.params.partcode.trim().toUpperCase();
-        const partsArr = part.split(' ');
-        await client.connect();
-        const coll = db.collection("uwm_stor_parts");
-        if(partsArr.length > 1){
-            const RegExPartsArr=[];
-            partsArr.forEach((p)=>{
-                const reStr =  '^' + p;
-                const re = new RegExp(reStr, 'i');
-                RegExPartsArr.push(re);
-            })
-            const result = await coll.find( { partCode: { $in: RegExPartsArr } })
-            .sort({warehouseBinLocation: 1, partCode: 1}).toArray();
-            res.json(result);
+//         const part = req.params.partcode.trim().toUpperCase();
+//         const partsArr = part.split(' ');
+//         await client.connect();
+//         const coll = db.collection("uwm_stor_parts");
+//         if(partsArr.length > 1){
+//             const RegExPartsArr=[];
+//             partsArr.forEach((p)=>{
+//                 const reStr =  '^' + p;
+//                 const re = new RegExp(reStr, 'i');
+//                 RegExPartsArr.push(re);
+//             })
+//             const result = await coll.find( { partCode: { $in: RegExPartsArr } })
+//             .sort({warehouseBinLocation: 1, partCode: 1}).toArray();
+//             res.json(result);
             
-        }
-        else{
-            const reStr = '^' + part;
-            const re = new RegExp(reStr, 'i');
-            const result = await coll.find( { partCode: { $in: [re]} } )
-                .sort({warehouseBinLocation: 1, partCode: 1}).toArray();
-            res.json(result)
-        }
-    }
-    catch(err){
-        if(err.message == 'Invalid query format'){
-            res.status(400).json({message: "Invalid syntax"})
-        }
-        else{res.status(500).json({message:"Error fetching data"})}
-    }
-})
+//         }
+//         else{
+//             const reStr = '^' + part;
+//             const re = new RegExp(reStr, 'i');
+//             const result = await coll.find( { partCode: { $in: [re]} } )
+//                 .sort({warehouseBinLocation: 1, partCode: 1}).toArray();
+//             res.json(result)
+//         }
+//     }
+//     catch(err){
+//         if(err.message == 'Invalid query format'){
+//             res.status(400).json({message: "Invalid syntax"})
+//         }
+//         else{res.status(500).json({message:"Error fetching data"})}
+//     }
+// })
 
 // Get part record with no auth
 router.get("/parts/:partCode", async (req, res)=>{ 
